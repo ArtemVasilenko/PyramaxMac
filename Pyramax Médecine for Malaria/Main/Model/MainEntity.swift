dc1dd0b377ac7cb09157cb46e6520f359590c644
Artem <artemvasilenko88@gmail.com>
2020-10-12 18:19:29 +0300

    work

---
+++ Pyramax Médecine for Malaria/Main/Model/MainEntity.swift
@@ -0,0 +1,11 @@
+//import Foundation
+
+    enum StoryBoardID: String {
+        case PresentationStartSB
+        case SelectCoverSB
+    }
+    
+    enum ControllerID: String {
+        case PresentationStartVC
+        case SelectCoverVC
+    }
